341beba09fabf117d1d8a76659ab1c78b15f4bcf
Max Katz <maxkatz6@outlook.com>
2025-06-09 18:41:05 -0700

    Missed IPlatformHandle

--- src/Avalonia.Controls.WebView.Core/Platform/IAndroidWebViewPlatformHandle.cs
+++ src/Avalonia.Controls.WebView.Core/Platform/IAndroidWebViewPlatformHandle.cs
@@ -3,7 +3,7 @@
 // ReSharper disable once CheckNamespace
 namespace Avalonia.Platform;
 
-public interface IAndroidWebViewPlatformHandle
+public interface IAndroidWebViewPlatformHandle : IPlatformHandle
 {
     /// <summary>
     /// Returns handle of the Android.Webkit.WebView Android object.
