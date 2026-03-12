43fc079b01946f1edd9f7f767f31777c560b4508
Max Katz <maxkatz6@outlook.com>
2026-03-12 14:24:45 +0900

    Make browser classes use file scoped namespaces

--- src/Avalonia.Controls.WebView.Core/Browser/BrowserIFrameAdapter.cs
+++ src/Avalonia.Controls.WebView.Core/Browser/BrowserIFrameAdapter.cs
@@ -8,132 +8,131 @@ using Avalonia.Browser;
 using Avalonia.Media;
 using Avalonia.Platform;
 
-namespace Avalonia.Controls.Browser
+namespace Avalonia.Controls.Browser;
+
+// Note: this adapter is not yet compatible with WASM multithreading.
+// In order to support that we need to use only async JS interop, but IWebViewAdapter API is not compatible with that.
+[SupportedOSPlatform("browser")]
+internal class BrowserIFrameAdapter : JSObjectControlHandle, IWebViewAdapter
 {
-    // Note: this adapter is not yet compatible with WASM multithreading.
-    // In order to support that we need to use only async JS interop, but IWebViewAdapter API is not compatible with that.
-    [SupportedOSPlatform("browser")]
-    internal class BrowserIFrameAdapter : JSObjectControlHandle, IWebViewAdapter
+    private static readonly Lazy<Task> s_importModule = new(WebViewInterop.EnsureLoaded);
+
+    private Action? _subscriptions;
+    private Uri? _lastSrc;
+
+    private BrowserIFrameAdapter(JSObject iframe) : base(iframe)
     {
-        private static readonly Lazy<Task> s_importModule = new(WebViewInterop.EnsureLoaded);
+    }
 
-        private Action? _subscriptions;
-        private Uri? _lastSrc;
+    public static async Task<WebViewAdapter.NativeWebViewAdapterBuilder> CreateBuilder(
+        BrowserWebViewEnvironmentRequestedEventArgs environmentArgs)
+    {
+        await s_importModule.Value;
+        var iframe = await WebViewInterop.CreateElement("iframe");
 
-        private BrowserIFrameAdapter(JSObject iframe) : base(iframe)
+        return (_, _) =>
         {
-        }
+            var adapter = new BrowserIFrameAdapter(iframe);
+            return new WebViewAdapter.AdapterWrapper(adapter, InitializeAsync(adapter, environmentArgs));
+        };
 
-        public static async Task<WebViewAdapter.NativeWebViewAdapterBuilder> CreateBuilder(
+        static async Task<IWebViewAdapter> InitializeAsync(
+            BrowserIFrameAdapter adapter,
             BrowserWebViewEnvironmentRequestedEventArgs environmentArgs)
         {
-            await s_importModule.Value;
-            var iframe = await WebViewInterop.CreateElement("iframe");
+            await adapter.InitializeAsync(environmentArgs);
+            return adapter;
+        }
+    }
 
-            return (_, _) =>
-            {
-                var adapter = new BrowserIFrameAdapter(iframe);
-                return new WebViewAdapter.AdapterWrapper(adapter, InitializeAsync(adapter, environmentArgs));
-            };
+    public Color DefaultBackground { set { } }
 
-            static async Task<IWebViewAdapter> InitializeAsync(
-                BrowserIFrameAdapter adapter,
-                BrowserWebViewEnvironmentRequestedEventArgs environmentArgs)
+    public void SizeChanged(PixelSize containerSize) { }
+
+    public void SetParent(IPlatformHandle parent) { }
+
+    public bool CanGoBack => WebViewInterop.CanGoBack(Object);
+
+    public bool CanGoForward => false;
+
+    public Uri Source
+    {
+        get
+        {
+            if (Uri.TryCreate(WebViewInterop.GetActualLocation(Object), UriKind.Absolute, out var location))
             {
-                await adapter.InitializeAsync(environmentArgs);
-                return adapter;
+                return location;
             }
+            return _lastSrc!;
         }
-    
-        public Color DefaultBackground { set { } }
+        set { Navigate(value); }
+    }
 
-        public void SizeChanged(PixelSize containerSize) { }
+    public event EventHandler<WebViewNavigationCompletedEventArgs>? NavigationCompleted;
+    public event EventHandler<WebViewNavigationStartingEventArgs>? NavigationStarted;
+    public event EventHandler<WebViewNewWindowRequestedEventArgs>? NewWindowRequested;
+    public event EventHandler<WebMessageReceivedEventArgs>? WebMessageReceived;
+    public event EventHandler<WebResourceRequestedEventArgs>? WebResourceRequested;
 
-        public void SetParent(IPlatformHandle parent) { }
+    public bool GoBack() => WebViewInterop.GoBack(Object);
 
-        public bool CanGoBack => WebViewInterop.CanGoBack(Object);
+    public bool GoForward() => WebViewInterop.GoForward(Object);
 
-        public bool CanGoForward => false;
+    public Task<string?> InvokeScript(string script)
+    {
+        return WebViewInterop.Eval(Object, script);
+    }
 
-        public Uri Source
-        {
-            get
+    public void Navigate(Uri url)
+    {
+        _lastSrc = url;
+        NavigationStarted?.Invoke(this, new WebViewNavigationStartingEventArgs { Request = url });
+        Object.SetProperty("src", url.AbsoluteUri);
+    }
+
+    public void NavigateToString(string text)
+    {
+        _lastSrc = new Uri("about:srcdoc");
+        Object.SetProperty("srcdoc", text);
+    }
+
+    public bool Refresh()
+    {
+        return WebViewInterop.Refresh(Object);
+    }
+
+    public bool Stop()
+    {
+        return WebViewInterop.Stop(Object);
+    }
+
+    public void Dispose()
+    {
+        _subscriptions?.Invoke();
+    }
+
+    internal static DetailedWebViewAdapterInfo GetBrowserInfo()
+    {
+        return new DetailedWebViewAdapterInfo(
+            WebViewAdapterType.BrowserIFrame,
+            WebViewEngine.Unknown,
+            IsSupported: OperatingSystem.IsBrowser(),
+            IsInstalled: OperatingSystem.IsBrowser(),
+            Version: null,
+            UnavailableReason: OperatingSystem.IsBrowser() ? null : "Not running in a browser environment.",
+            SupportedScenarios: WebViewEmbeddingScenario.NativeControlHost);
+    }
+
+    private Task InitializeAsync(BrowserWebViewEnvironmentRequestedEventArgs environmentArgs)
+    {
+        var unsub = WebViewInterop.Subscribe(Object,
+            src => NavigationCompleted?.Invoke(this, new WebViewNavigationCompletedEventArgs
             {
-                if (Uri.TryCreate(WebViewInterop.GetActualLocation(Object), UriKind.Absolute, out var location))
-                {
-                    return location;
-                }
-                return _lastSrc!;
-            }
-            set { Navigate(value); }
-        }
+                Request = Uri.TryCreate(src, UriKind.Absolute, out var request) ? request : null
+            }));
 
-        public event EventHandler<WebViewNavigationCompletedEventArgs>? NavigationCompleted;
-        public event EventHandler<WebViewNavigationStartingEventArgs>? NavigationStarted;
-        public event EventHandler<WebViewNewWindowRequestedEventArgs>? NewWindowRequested;
-        public event EventHandler<WebMessageReceivedEventArgs>? WebMessageReceived;
-        public event EventHandler<WebResourceRequestedEventArgs>? WebResourceRequested;
-
-        public bool GoBack() => WebViewInterop.GoBack(Object);
-
-        public bool GoForward() => WebViewInterop.GoForward(Object);
-
-        public Task<string?> InvokeScript(string script)
-        {
-            return WebViewInterop.Eval(Object, script);
-        }
-
-        public void Navigate(Uri url)
-        {
-            _lastSrc = url;
-            NavigationStarted?.Invoke(this, new WebViewNavigationStartingEventArgs { Request = url });
-            Object.SetProperty("src", url.AbsoluteUri);
-        }
-
-        public void NavigateToString(string text)
-        {
-            _lastSrc = new Uri("about:srcdoc");
-            Object.SetProperty("srcdoc", text);
-        }
-
-        public bool Refresh()
-        {
-            return WebViewInterop.Refresh(Object);
-        }
-
-        public bool Stop()
-        {
-            return WebViewInterop.Stop(Object);
-        }
-
-        public void Dispose()
-        {
-            _subscriptions?.Invoke();
-        }
-
-        internal static DetailedWebViewAdapterInfo GetBrowserInfo()
-        {
-            return new DetailedWebViewAdapterInfo(
-                WebViewAdapterType.BrowserIFrame,
-                WebViewEngine.Unknown,
-                IsSupported: OperatingSystem.IsBrowser(),
-                IsInstalled: OperatingSystem.IsBrowser(),
-                Version: null,
-                UnavailableReason: OperatingSystem.IsBrowser() ? null : "Not running in a browser environment.",
-                SupportedScenarios: WebViewEmbeddingScenario.NativeControlHost);
-        }
-
-        private Task InitializeAsync(BrowserWebViewEnvironmentRequestedEventArgs environmentArgs)
-        {
-            var unsub = WebViewInterop.Subscribe(Object,
-                src => NavigationCompleted?.Invoke(this, new WebViewNavigationCompletedEventArgs
-                {
-                    Request = Uri.TryCreate(src, UriKind.Absolute, out var request) ? request : null
-                }));
-
-            _subscriptions = unsub;
-            return Task.CompletedTask;
-        }
+        _subscriptions = unsub;
+        return Task.CompletedTask;
     }
 }
 #endif
